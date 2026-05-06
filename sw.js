cede1b80aa4ac38bd5b96c45e15b9dd50151bc1b
b33n-tech <benjamin.griot@outlook.fr>
2026-05-06 16:36:58 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "langbridge-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./logo.svg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
